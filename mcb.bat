1de4f29f469e73b58914aecf193c74f96e3db6f0
RayNieva <raynieva@gmail.com>
2016-07-04 12:23:04 -0400

    new file:   mcb.bat 	new file:   mcb.pyw

---
+++ mcb.bat
@@ -0,0 +1,6 @@
+@rem ;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+@rem ;; If you want to create a file, visit that file with C-x C-f,
+@rem ;; then enter the text in that file's own buffer.
+
+@cd c:\users\ray\bin
+@python c:\users\ray\bin\mcb.pyw %*
